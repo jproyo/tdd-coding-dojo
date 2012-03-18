76b6693c76752cc87799ae8ae035a63552fdf84c
jproyo <juanpablo.royo@gmail.com>
2012-03-18 18:45:19 -0300

    General Refactor non working rules

--- app/working_dates_calendar.rb
+++ app/working_dates_calendar.rb
@@ -7,31 +7,11 @@ class WorkingDatesCalendar
 	end
 
 	def is_non_working_date(date)
-		true if is_non_working_day?(date) or is_non_working_month_day?(date) or is_non_working_specific_date?(date)
+		true if @non_working_rules.any? {|rule| rule.is_non_working(date) }
 	end
 
-	def is_non_working_specific_date?(date)
-		@non_working_rules.any? {|rule| rule.is_non_working(date) }
-	end
-
-	def is_non_working_month_day?(date)
-		@non_working_rules.any? {|rule| rule.is_non_working(date) }
-	end
-
-	def is_non_working_day?(date)
-		@non_working_rules.any? {|rule| rule.is_non_working(date) }
-	end
-
-	def add_non_working_date(rule_date)
-		@non_working_rules << rule_date
-	end
-
-	def add_non_working_day(rule_day)
-		@non_working_rules << rule_day
-	end
-
-	def add_non_working_month_day(rule_month)
-		@non_working_rules << rule_month
+	def add_non_working_rule(rule)
+		@non_working_rules << rule
 	end
 
 
